5efeac0b044c7d95c62634f8c648650c7f41da77
i-i-ijul <haqqikamvret@gmail.com>
2025-06-26 10:08:12 +0700

    membuat left dan right child di new node menjadi null

--- tree.cpp
+++ tree.cpp
@@ -34,5 +34,14 @@ class BinaryTree
 
         //step 1 : allocate memory for the new node
         Node *newNode = new Node();
+
+        //step 2 : assign value to the data field of new node
+        newNode->info = x;
+
+        //step 3: make the left n right child of the new node point to null
+        newNode->leftchild = nullptr;
+        newNode->rightchild = nullptr;
+
+        //step 4: locate the node which will be the parent 
     }
 }
