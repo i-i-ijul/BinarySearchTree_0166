917ca23243fa1aba19fc6bacfed9c3a7cf6e0ec9
i-i-ijul <haqqikamvret@gmail.com>
2025-06-26 09:58:45 +0700

    menambahkan  library

--- tree.cpp
+++ tree.cpp
@@ -3,5 +3,15 @@ using namespace std;
 
 class Node
 {
+ public:
+    int info;
+    Node *leftchild;
+    Node *rightchild;
     
-}
+    //constructor for the node class
+    Node()
+    {
+        leftchild = nullptr; //initialize left child to null
+        rightchild = nullptr; //initialize right child to null
+    }
+};
